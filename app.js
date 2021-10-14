0a2cd2002e636d49bf87d2bf7584403d57ffddb5
rick019 <87342919+rick019@users.noreply.github.com>
2021-10-13 22:18:17 -0300

    Pagina HTML + CSS + Javascript
    Cadastro rodape funcionando com o JavaScript

---
+++ app.js
@@ -0,0 +1,5 @@
+function Cadastro(text){
+    let EnviarCadastroRodape = document.querySelector("#cadastro")
+    document.querySelector
+    document.querySelector("#resposta").innerHTML = "Cadastro Realizado com Sucesso!"
+}
