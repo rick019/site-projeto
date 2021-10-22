77d299a4efc6bcbf237d840b027ee70190c0d87a
clapedro <clayton03e@gmail.com>
2021-10-22 01:45:19 +0100

    Home + App + css

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ function Cadastro(text){
     document.querySelector
     document.querySelector("#resposta").innerHTML = "Cadastro Realizado com Sucesso!"
 }
+
+
+function MostrarReserva(){
+    let campo = document.querySelector("#reserve")
+    if(campo.style.display == "inline"){
+        campo.style.display = "none"
+    }else{
+        campo.style.display = "inline"
+    }
+    
+}
+
+
+
+function EnviarReserva(text){
+    let EnviarReserva = document.querySelector("#reserve")
+    
+    document.querySelector
+    document.querySelector("#resposta2").innerHTML = "Cadastro Realizado com Sucesso!"
+}
